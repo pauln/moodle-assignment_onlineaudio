28bc70db67d48efe53441163ac969dccf1847a1d
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-03-26 12:46:30 +1300

    Add French translation (translation supplied by Luiggi Sansonetti)

---
+++ lang/fr/assignment_onlineaudio.php
@@ -0,0 +1,23 @@
+<?php
+
+$string['pluginname'] = 'Enregistrement Audio en Ligne';
+$string['typeonlineaudio'] = 'Enregistrement Audio en Ligne';
+$string['onlineaudiodownload'] = 'Vous pouvez <a href=\'$a\'>télécharger</a> le fichier et le lire avec n\'importe quel lecteur prenant en charge le format mp3.';
+$string['onlineaudioplaying'] = 'Lecture $a';
+$string['uploadafile'] = 'Charger un enregistrement existant :';
+$string['filelimitreached'] = 'Vous avez atteint le nombre maximum de fichiers pour ce dépôt.';
+$string['advanceduploadafile'] = 'Téléchargement avancé';
+$string['upload'] = 'Télécharger ce fichier';
+$string['uploadnote'] = '<strong>Note :</strong> Seuls les fichiers au format mp3, wma ou wav peuvent être téléchargés<br />';
+$string['allowupload'] = 'Autoriser le téléchargement manuel';
+$string['allowupload_help'] = 'Autoriser les étudiants à télécharger leurs propres fichiers (formats pris en charge) en plus de l\'utilisation de l\'enregistreur.';
+$string['makenewrecording'] = 'Enregistrer un nouveau fichier audio :';
+$string['onlineaudiosilencenote'] = '<strong>Merci de noter :</strong> Les périodes de silence sont automatiquement sautées. Si vous venez d\'enregistrer cette séquence, et qu\'une partie de votre séquence (non silencieuse) audio est manquante, merci de tourner le volume du microphone en utilisant le bouton \"Paramètres ... \" dans l\'enregistreur audio et essayez à nouveau.';
+$string['filetypeerror'] = 'Seules les extensions mp3, wma or wav sont supportées. Merci de réessayer.';
+$string['defaultname'] = 'Nom de fichier par défaut';
+$string['defaultname_help'] = 'Cette option peut être utilisée pour prédéfinir le nom de fichier basé sur un modèle. Le changement de nom de fichier prédéfini peut être bloqué en réglant l\'option "Autoriser les étudiants à changer de nom de fichier" à "Non".';
+$string['nodefaultname'] = 'Rien (vide)';
+$string['defaultname1'] = 'username_assignment_course_date';
+$string['defaultname2'] = 'fullname_assignment_course_date';
+$string['allownameoverride'] = 'Autoriser les étudiants à changer de nom de fichier';
+$string['allownameoverride_help'] = 'Si l\'option est activée, les étudiants peuvent remplacer le nom de fichier par défaut avec l\'un de leur choix. Cette option n\'a aucun effet si le "Nom de fichier par défaut" est réglé sur "Rien (vide)" puisqu\'un nom doit être spécifié.';
