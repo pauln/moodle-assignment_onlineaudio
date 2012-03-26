bc42abf0dbe7f0f361994aa6c01e3efa513e867c
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-03-26 13:30:08 +1300

    Add French translation (translation supplied by Luiggi Sansonetti)

---
+++ lang/fr_utf8/assignment_onlineaudio.php
@@ -0,0 +1,16 @@
+<?php
+
+$string['typeonlineaudio'] = 'Enregistrement Audio en Ligne';
+$string['onlineaudiodownload'] = 'Vous pouvez <a href=\'$a\'>télécharger</a> le fichier et le lire avec n\'importe quel lecteur prenant en charge le format mp3.';
+$string['onlineaudioplaying'] = 'Lecture $a';
+$string['uploadafile'] = 'Charger un enregistrement existant :';
+$string['allowupload'] = 'Autoriser le téléchargement manuel';
+$string['allowuploadhelp'] = 'Autoriser les étudiants à télécharger leurs propres fichiers (formats pris en charge) en plus de l\'utilisation de l\'enregistreur.';
+$string['makenewrecording'] = 'Enregistrer un nouveau fichier audio :';
+$string['onlineaudiosilencenote'] = '<strong>Merci de noter :</strong> Les périodes de silence sont automatiquement sautées. Si vous venez d\'enregistrer cette séquence, et qu\'une partie de votre séquence (non silencieuse) audio est manquante, merci de tourner le volume du microphone en utilisant le bouton \"Paramètres ... \" dans l\'enregistreur audio et essayez à nouveau.';
+$string['filetypeerror'] = 'Seules les extensions mp3, wma or wav sont supportées. Merci de réessayer.';
+$string['defaultname'] = 'Nom de fichier par défaut';
+$string['nodefaultname'] = 'Rien (vide)';
+$string['defaultname1'] = 'username_assignment_course_date';
+$string['defaultname2'] = 'fullname_assignment_course_date';
+$string['allownameoverride'] = 'Autoriser les étudiants à changer de nom de fichier';
