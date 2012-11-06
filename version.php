f1c91282800feaa9e162cdd8f3cfa5ac83bb7758
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-11-07 10:22:45 +1300

    Version bump

--- version.php
+++ version.php
@@ -5,6 +5,8 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version   = 2011102006;
+$plugin->version   = 2011102007;
+$plugin->release   = "1.1.1";
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400;
 $plugin->component = 'assignment_onlineaudio';
