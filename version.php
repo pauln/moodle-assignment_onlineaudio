97451c1bc2d57637fd3edffebded58dddeac4113
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-04-15 12:00:31 +1200

    Version bump to 1.1.2

--- version.php
+++ version.php
@@ -5,8 +5,8 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version   = 2011102007;
-$plugin->release   = "1.1.1";
+$plugin->version   = 2011102008;
+$plugin->release   = "1.1.2";
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2010112400;
 $plugin->component = 'assignment_onlineaudio';
