71d0d3c3bf14490b9e1e1dda8883f98992cc967b
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-04-15 12:05:55 +1200

    Version bump

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version   = 2011090805;
+$plugin->version   = 2011090806;
 $plugin->requires  = 2007101590;
 
 
